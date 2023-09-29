069ae8711dde528312ff9f5dc5408c17c63a6f13
jill64 <143883742+jill64@users.noreply.github.com>
2023-09-29 11:45:41 +0000

    fix: viewer request handler

--- types/edge/handler/ViewerRequestHandler.ts
+++ types/edge/handler/ViewerRequestHandler.ts
@@ -3,16 +3,11 @@ import { LambdaResponseCallback } from '../../lambda/LambdaResponseCallback.js'
 import { ViewerRequestEvent } from '../event/ViewerRequestEvent.js'
 import { EdgeIncludeBodyOption } from '../field/EdgeIncludeBodyOption.js'
 import { EdgeRequestReturnValue } from '../field/EdgeRequestReturnValue.js'
-import { OriginType } from '../field/OriginType.js'
 
 export type ViewerRequestHandler<
-  Origin extends OriginType,
   IncludeBody extends EdgeIncludeBodyOption = undefined
 > = (
   event: ViewerRequestEvent<IncludeBody>,
   context: LambdaRequestContext,
-  callback: LambdaResponseCallback<EdgeRequestReturnValue<Origin>>
-) =>
-  | Promise<EdgeRequestReturnValue<Origin> | void>
-  | EdgeRequestReturnValue<Origin>
-  | void
+  callback: LambdaResponseCallback<EdgeRequestReturnValue>
+) => Promise<EdgeRequestReturnValue | void> | EdgeRequestReturnValue | void
