f4e80fdd53233507b40dc64e33a4b89d8bf0d372
jill64 <intents.turrets0h@icloud.com>
2023-09-29 18:48:02 +0900

    add request handler

---
+++ types/edge/handler/ViewerRequestHandler.ts
@@ -0,0 +1,15 @@
+import { LambdaRequestContext } from '../../lambda/LambdaRequestContext.js'
+import { LambdaResponseCallback } from '../../lambda/LambdaResponseCallback.js'
+import { ViewerRequestEvent } from '../event/ViewerRequestEvent.js'
+import { EdgeIncludeBodyOption } from '../field/EdgeIncludeBodyOption.js'
+import { EdgeRequestReturnValue } from '../field/EdgeRequestReturnValue.js'
+import { OriginType } from '../field/OriginType.js'
+
+export type ViewerRequestHandler<
+  Origin extends OriginType,
+  IncludeBody extends EdgeIncludeBodyOption = undefined
+> = (
+  event: ViewerRequestEvent<IncludeBody>,
+  context: LambdaRequestContext,
+  callback: LambdaResponseCallback<EdgeRequestReturnValue<Origin>>
+) => Promise<EdgeRequestReturnValue<Origin>> | EdgeRequestReturnValue<Origin>
